a1bab7238877f8254737667488c0c1d11d90b68c
alkor63 <alkor@medi-spa.ru>
2022-12-13 01:21:36 +0300

    Java Core 1 task 3

--- src/Car.java
+++ src/Car.java
@@ -7,6 +7,13 @@ public class Car {
     double engineVolume;
 
     public Car(String brand, String model, int year, String country, String color, double engineVolume) {
+
+        if (year <= 0) year = 2000;
+        if (engineVolume <= 0) engineVolume = 1.5;
+        if (nullString(color)) color = "белый";
+        if (nullString(brand)) brand = "default";
+        if (nullString(model)) model = "default";
+        if (nullString(country)) country = "default";
         this.brand = brand;
         this.model = model;
         this.year = year;
@@ -14,7 +21,9 @@ public class Car {
         this.color = color;
         this.engineVolume = engineVolume;
     }
-
+    public boolean nullString(String s){
+        return (s == null || s.equals(""));
+    }
     @Override
     public String toString() {
         return "Автомобиль " + brand + " " + model + ", " + year +
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         cars[1] = new Car("Audi","A8 50 L TDI quattro",2020,"Германия","черный",3.0);
         cars[2] = new Car("BMW","Z8",2015,"Германия","черный",3.0);
         cars[3] = new Car("Kia","Sportage 4-го поколения",2018,"Южная Корея","красный",2.4);
-        cars[4] = new Car("Hyundai","Avante",2016,"Южная Корея","оранжевый",1.6);
+        cars[4] = new Car("","Avante",-2016,"Южная Корея",null,-1.6);
 
         for (Car car: cars)
             System.out.println(car);
